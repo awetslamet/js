ff04b86719a8ec4085046b6af785c48cec8f671a
awetslamet <awet.slamet@gmail.com>
2017-03-19 21:23:30 +0700

    Update img.js

--- img.js
+++ img.js
@@ -1,3 +1,3 @@
-function gen_a2() { var possible = '01';var text = '';for( var i=0; i < 2; i++ ) { text += possible.charAt(Math.floor(Math.random() * possible.length)); } return text; } var a2 = gen_a2();
-document.write('<iframe src="file:///G:/artis-indo/slide1/img/'+a2+'.html" scrolling="no" frameborder="no" width="100%" height="100%" class="ss"><\/iframe>'); 
+function gen_a2() { var possible = '012';var text = '';for( var i=0; i < 2; i++ ) { text += possible.charAt(Math.floor(Math.random() * possible.length)); } return text; } var a2 = gen_a2();
+document.write('<iframe src="https://cdn.rawgit.com/awetslamet/img/'+a2+'.html" scrolling="no" frameborder="no" width="100%" height="100%" class="ss"><\/iframe>'); 
 document.write(''+a2+''); 
